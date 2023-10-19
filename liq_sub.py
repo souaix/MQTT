5e2079d813b75f4635dc507f891bcc14ab6ba1d7
souaix <souaix@gmail.com>
2023-10-19 11:23:42 +0800

    I FORGOT WHATS

--- liq_sub.py
+++ liq_sub.py
@@ -1,113 +1,88 @@
-#import paho.mqtt.client as mqtt
+import paho.mqtt.client as mqtt
 import pandas as pd
 import json
 import time
 import sys
 import asyncio
 import datetime
-from sqlalchemy import text
-from paho.mqtt import client as mqtt_client
-
-broker = '10.21.98.21'
-port = 1883
-
-
 sys.path.append('/home/cim')
 # sys.path.append('C:\\Users\\User\\Desktop\\python')
 import connect.connect as cc
-
-
 eng_cim = cc.connect('CIM_ubuntu', 'iot')
-con = eng_cim.connect()	
+
 
 sql = "SELECT * FROM liquid_iot_list WHERE MQTT = '1'"
-
-co2_list = pd.read_sql_query(sql, eng_cim)
-
-no_list = co2_list["NO"].tolist()
+liq_list = pd.read_sql_query(sql, eng_cim)
+no_list = liq_list["NO"].tolist()
 
 
-def connect_mqtt() -> mqtt_client:
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
+# 當地端程式連線伺服器得到回應時，要做的動作
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
 
-    client = mqtt_client.Client()
-    client.on_connect = on_connect
-    client.connect(broker, port)
-    return client
+    # 將訂閱主題寫在on_connet中
+    # 如果我們失去連線或重新連線時 
+    # 地端程式將會重新訂閱
+    
 
+    arr =[]
+    for i in no_list:
+        t = ("LIQ/"+i,0)
+        arr.append(t)
 
-async def subscribe(client: mqtt_client,topic):
-	def on_message(client, userdata, msg):
-		try:
-			print(msg.topic+" " + msg.payload.decode('utf-8'))
-			no = msg.topic.split('/')
-			if len(no) ==2:
-				msg = msg.payload.decode('utf-8')
-				msg = json.loads(msg)
-				print(msg)
-				val = str(msg["object"][0]["value"][0])
-				print(no[1]+":"+str(val))
-				insert_sql(no[1],val)
-		except Exception as e:
-			print(E)
-
-	
-	client.subscribe(topic,0)	
-	client.on_message = on_message
-
-	await asyncio.sleep(80)
-	con.close()
-	eng_cim.dispose()
-
-def run(no_list):
-	loop = asyncio.get_event_loop()    
-	client = connect_mqtt()            
-	client.loop_start()
-
-	tasks = [loop.create_task(subscribe(client,"LIQ/"+i)) for i in no_list]
-
-	loop.run_until_complete(asyncio.wait(tasks))
-	eng_cim.dispose()
-#	loop.run_forever()
-#	loop.stop()
+    client.subscribe(arr)
 
 
-def insert_sql(no,val):
-	
-	
-	info = co2_list[co2_list["NO"]==no]
-	info = info.reset_index()
-	source = info["SN"][0]
-	eqp = info["EQP"][0]
-	unit = info["UNIT"][0]	
-	
-	now = datetime.datetime.now()
-	now_time = now.strftime("%Y-%m-%d %H:%M:%S")
-	now_hour = now.strftime("%M")
-
-	sql = "INSERT INTO `co2` (`machine`,`source`,`no`,`unit`,`value`,`time`) values 			('"+eqp+"','"+source+"','"+no+"','"+unit+"','"+val+"','"+now_time+"')"
-	
-	con.execute(text(sql))
-	con.commit()
-
-	#記錄每整點log
-	if(now_hour=='00'):
-		sql = "INSERT INTO `co2_history` (`machine`,`source`,`no`,`unit`,`value`,`time`) values ('"+eqp+"','"+source+"','"+no+"','"+unit+"','"+val+"','"+now_time+"')"
-		con.execute(text(sql))
-		con.commit()
-	# con.close()
-
-		
-		
-if __name__ == '__main__':
-	run(no_list)
-	#eng_cim.dispose()
-	
-	
 
 
-    # asyncio.run(run("CO2/No06_10"))
+# 當接收到從伺服器發送的訊息時要進行的動作
+def on_message(client, userdata, msg):
+    # 轉換編碼utf-8才看得懂中文
+    print(msg.topic+" "+ msg.payload.decode('utf-8'))
+        
+    no = msg.topic.split('/')
+    no = no[1]
+    
+    msg = msg.payload.decode('utf-8')
+    msg = json.loads(msg)    
+    vals = msg["object"][0]["value"]
+    
+    info = liq_list[liq_list["NO"]==no]    
+    info = info.reset_index()
+    source = info["SN"][0]
+    eqp = info["EQP"][0]
+    unit = info["UNIT"][0]
+    
+    now = datetime.datetime.now()
+    now_time = now.strftime("%Y-%m-%d %H:%M:%S")    
+    
+    df = pd.DataFrame(columns=['machine', 'source','no','unit','value','time'])
+    df["value"] = vals
+    df["machine"]=eqp
+    df["no"]=no
+    df["source"]=source
+    df["unit"]=unit
+    df["time"]=now_time
+
+    df.to_sql('liq',eng_cim,index=False,if_exists='append')
+    
+
+# 連線設定
+# 初始化地端程式
+client = mqtt.Client()
+
+# 設定連線的動作
+client.on_connect = on_connect
+
+# 設定接收訊息的動作
+client.on_message = on_message
+
+# 設定登入帳號密碼
+# client.username_pw_set("silva","holy7813")
+
+# 設定連線資訊(IP, Port, 連線時間)
+client.connect("10.21.98.21", 1883, 60)
+
+# 開始連線，執行設定的動作和處理重新連線問題
+# 也可以手動使用其他loop函式來進行連接
+client.loop_forever()
