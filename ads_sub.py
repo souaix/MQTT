a00882342154b1f2a3d1b62f194a16732b00de05
souaix <souaix@gmail.com>
2024-06-24 10:35:54 +0800

    ADD_SUB

--- ads_sub.py
+++ ads_sub.py
@@ -1,4 +1,3 @@
-#import paho.mqtt.client as mqtt
 import pandas as pd
 import json
 import time
@@ -20,11 +19,12 @@ import connect.connect as cc
 eng_cim = cc.connect('CIM_ubuntu', 'iot')
 con = eng_cim.connect()	
 
-sql = "SELECT * FROM dscw_iot_list WHERE MQTT = '1'"
+sql = "SELECT * FROM ads_iot_list WHERE MQTT = '1'"
 
-eq_list = pd.read_sql_query(sql, eng_cim)
+iot_list = pd.read_sql_query(sql, eng_cim)
 
-no_list = eq_list["NO"].tolist()
+no_list = iot_list["NO"].tolist()
+
 
 
 def connect_mqtt() -> mqtt_client:
@@ -40,20 +40,19 @@ def connect_mqtt() -> mqtt_client:
     return client
 
 
+
 async def subscribe(client: mqtt_client,topic):
 	def on_message(client, userdata, msg):
 		try:
 			print(msg.topic+" " + msg.payload.decode('utf-8'))
-			no = msg.topic.split('/')
-			if len(no) ==2:
-				msg = msg.payload.decode('utf-8')
-				msg = json.loads(msg)
-				val = str(msg["object"][0]["value"][0])
-				#print(no[1]+":"+str(val))
-				insert_sql(no[1],val)
-
-		except Exception as E:
-			print(E)
+			no = msg.topic
+			msg = msg.payload.decode('utf-8')
+			msg = json.loads(msg)
+			val = str(msg["object"][0]["value"][0])
+			print(no+":"+str(val))
+			insert_sql(no,val)
+		except Exception as e:
+			print(e)
 
 	
 	client.subscribe(topic,0)	
@@ -63,12 +62,13 @@ async def subscribe(client: mqtt_client,topic):
 	con.close()
 	eng_cim.dispose()
 
+
 def run(no_list):
 	loop = asyncio.get_event_loop()    
 	client = connect_mqtt()            
 	client.loop_start()
 
-	tasks = [loop.create_task(subscribe(client,"DSCW/"+i)) for i in no_list]
+	tasks = [loop.create_task(subscribe(client,i)) for i in no_list]
 
 	loop.run_until_complete(asyncio.wait(tasks))
 	eng_cim.dispose()
@@ -77,25 +77,21 @@ def run(no_list):
 
 
 def insert_sql(no,val):
+	
 
-    info = eq_list[eq_list["NO"]==no]
-    info = info.reset_index(drop=True)
-    print(info)
+    info = iot_list[iot_list["NO"]==no]
+    info = info.reset_index()
     source = info["SN"][0]
+    eqp = info["EQP"][0]
     unit = info["UNIT"][0]	
-    machine = info["EQP"][0]
 
     now = datetime.datetime.now()
     now_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    now_hour = now.strftime("%M")
 
-    df = pd.DataFrame({ "machine":machine,
-                        "source":source,
-                        "no":no,
-                        "unit":unit,
-                        "value":val,
-                        "time":now_time},index=[0])
-
-    df.to_sql('dscw',eng_cim,index=False,if_exists='append')
+    sql = "INSERT INTO `ads` (`machine`,`source`,`no`,`unit`,`value`,`time`) values 			('"+eqp+"','"+source+"','"+no+"','"+unit+"','"+val+"','"+now_time+"')"
+    con.execute(text(sql))
+    #con.commit()
 
 
 		
@@ -103,8 +99,3 @@ def insert_sql(no,val):
 if __name__ == '__main__':
 	run(no_list)
 	#eng_cim.dispose()
-	
-	
-
-
-    # asyncio.run(run("CO2/No06_10"))
